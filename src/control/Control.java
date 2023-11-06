431cc64a99a815fa06cf3b243a28e201c3f78878
Jean Carlos Santoya <jeancarlosodi@gmail.com>
2023-11-06 16:38:55 -0500

    Se agrega validación (#20)

--- src/control/Control.java
+++ src/control/Control.java
@@ -4,6 +4,8 @@ import model.Calculator;
 import model.Context;
 import vista.Ventana;
 
+import javax.swing.*;
+
 public class Control {
     private final Ventana ventana;
     private final Context ctx;
@@ -15,8 +17,22 @@ public class Control {
     }
 
     public void calcular() {
-        calc.setExpression(ventana.getExpression()); // set the expression to evaluate
-        calc.setContext(ctx); // configure the calculator with the model.Context
-        ventana.showAnswer(String.valueOf(calc.evaluate())); // Display the result
+        String expresion = ventana.getExpression();
+        if(expresion.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Por favor ingrese una expresión",
+                    "Expresión vacía", JOptionPane.ERROR_MESSAGE);
+        }
+        else {
+            calc.setExpression(expresion); // set the expression to evaluate
+            calc.setContext(ctx); // configure the calculator with the model.Context
+            try {
+                ventana.showAnswer(String.valueOf(calc.evaluate())); // Display the result
+            }
+            catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Por favor ingrese una expresión válida",
+                        "Expresión inválida", JOptionPane.ERROR_MESSAGE);
+                e.printStackTrace();
+            }
+        }
     }
 }
